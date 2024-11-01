99daf6dc5236c60f82a07ea067d6fac75624561a
reomiya <k021g1153@g.neec.ac.jp>
2024-11-01 16:59:13 +0900

    playerScript
    プレイヤー挙動のスクリプトを作成(未完)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public float mS = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = -transform.right * mS;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = transform.right * mS;
+        }
     }
 }
