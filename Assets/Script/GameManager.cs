b0898fcf8d82bb788140a4f06ab4d33bfdda8dc8
etigoshun <94036031+etigoshun@users.noreply.github.com>
2024-12-06 15:11:12 +0900

    タイトルのボタンの追加とそれぞれのシーンに遷移する(遊び方のみゲームオーバーに行く)

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -9,4 +9,13 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(nextScene);
     }
+
+    public void GameEnd()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+    }
 }
