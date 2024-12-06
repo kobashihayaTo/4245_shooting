75c1d4c14c17407315d2140d8da6cf1fe33d10a9
shota <k021g1403@g.neec.ac.jp>
2024-12-06 14:36:53 +0900

    Switcherの追加
    カメラの切り替え

--- Assets/Script/SceneSwitcher.cs
+++ Assets/Script/SceneSwitcher.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SceneSwitter : MonoBehaviour
 {
+    public bool IsMode = false; // falseが配置モード trueがプレイモード
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        IsMode = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void ModeChenge()
+    {
+        IsMode = true;
+    }
+
+    public bool GetterIsMode()
+    {
+        return IsMode;
+    }
+
 }
