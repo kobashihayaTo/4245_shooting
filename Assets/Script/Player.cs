4c73b0ec021428f2cbba8be1d3d90264887af7c8
kobashihayaTo <k021g1128@g.neec.ac.jp>
2024-12-09 16:15:20 +0900

    プッシュ時のエラーの排除した

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();  // rigidbodyを取得 
+        rb = gameObject.GetComponent<Rigidbody>();  // rigidbodyを取征E
         animator = GetComponent<Animator>();
     }
 
@@ -28,25 +28,25 @@ public class Player : MonoBehaviour
 
         if (player_pos.position.x >= -4.0f) 
         {
-            Debug.Log("左　通ってる");
+           
             if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
             {
-                rb.velocity = new Vector3(-10.0f, 0.0f, 0.0f); // 値を設定
+                rb.velocity = new Vector3(-10.0f, 0.0f, 0.0f); // 値を設宁E
                                                                //transform.position -= speed * transform.right * Time.deltaTime;
             }
         }
         if(player_pos.position.x <= 4.0f)
         {
-            Debug.Log("右　通ってる");
+
             if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             {
-                rb.velocity = new Vector3(10.0f, 0.0f, 0.0f); // 値を設定
+                rb.velocity = new Vector3(10.0f, 0.0f, 0.0f); // 値を設宁E
                                                               //transform.position += speed * transform.right * Time.deltaTime;
             }
             //rb.velocity.x = 3.0f;
         }
 
-        // アニメーション用のフラグ管理
+        // アニメーション用のフラグ管琁E
         if (scene.IsMode == true)
         {
             animator.SetBool("IsActive", true);
