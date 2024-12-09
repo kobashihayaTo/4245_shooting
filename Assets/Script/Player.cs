a196385755627dbd47638c9ff37a984f4c3ff8eb
kobashihayaTo <k021g1128@g.neec.ac.jp>
2024-12-09 12:43:00 +0900

    敵と自機の移動範囲の制限処理の完成

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,6 +6,8 @@ using UnityEngine.Scripting.APIUpdating;
 public class Player : MonoBehaviour
 {
     private Rigidbody rb;
+    [SerializeField]
+    private Transform player_pos;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +20,27 @@ public class Player : MonoBehaviour
     {
         rb.velocity = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = new Vector3(-10.0f, 0.0f, 0.0f); // 値を設定
-                                                           //transform.position -= speed * transform.right * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            rb.velocity = new Vector3(10.0f, 0.0f, 0.0f); // 値を設定
-                                                          //transform.position += speed * transform.right * Time.deltaTime;
-        }
 
+
+        if (player_pos.position.x >= -4.0f) 
+        {
+            Debug.Log("左　通ってる");
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                rb.velocity = new Vector3(-10.0f, 0.0f, 0.0f); // 値を設定
+                                                               //transform.position -= speed * transform.right * Time.deltaTime;
+            }
+        }
+        if(player_pos.position.x <= 4.0f)
+        {
+            Debug.Log("右　通ってる");
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = new Vector3(10.0f, 0.0f, 0.0f); // 値を設定
+                                                              //transform.position += speed * transform.right * Time.deltaTime;
+            }
+            //rb.velocity.x = 3.0f;
+        }
+        Debug.Log("velo:" + player_pos.position.x);
     }
 }
